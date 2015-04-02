df14327c3ff58d882e2450f448c9f98c314a9142
Jake <jakemny@gmail.com>
2015-04-02 14:49:13 -0400

    fixing webhook

--- app/controllers.php
+++ app/controllers.php
@@ -7,7 +7,7 @@ include "helpers.php";
 // API Endpoints
 
 function github_push()  {
-    $text = "" . shell_exec("git pull");
+    $text = "" . shell_exec("sudo git pull");
     require "Twilio/Services/Twilio.php";
     $AccountSid = "ACbd652dd257ef5f7fdbf246a6e7af8d3a";
     $AuthToken = "e22f767658650152da61ff7dc93ad57e";
